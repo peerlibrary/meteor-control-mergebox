f8e68b8fe0543ae7129d4f5bec5c9a78ae01f6a5
Mitar <mitar.git@tnode.com>
2015-12-20 01:24:31 +0100

    Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Control mergebox of publish endpoints",
-  version: '0.1.0',
+  version: '0.1.1',
   name: 'peerlibrary:control-mergebox',
   git: 'https://github.com/peerlibrary/meteor-control-mergebox.git'
 });
